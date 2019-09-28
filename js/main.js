c9fc154b4557064c0e7d74f56279525dcc4dbd45
susanto_tm <susanto.tm@gmail.com>
2019-09-28 18:07:57 -0400

    Officer Page Updated with Templates

--- js/main.js
+++ js/main.js
@@ -6,7 +6,10 @@ Navbar Scroll Function Animation
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-	if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+	var nav_primary = $('nav.primary');
+	var nav_height = nav_primary.height();
+
+	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
 		$("#permias-logo").addClass("logo-shrink")
 			.removeClass("logo");
 		$("nav").addClass('nav-scroll');
@@ -14,6 +17,14 @@ function scrollFunction() {
 			.addClass("scroll-h1");
 		$("div.menu").addClass('menu-right');
 		$("div.menu a").addClass('menu-item');
+
+		$('article div.officer-header').addClass("header-scroll");
+
+		// while (nav_height > 100) {
+		// 	nav_primary.height(nav_height - 2);
+		// 	nav_height -= 2;
+		// }
+
 	}
 	else {
 		$("#permias-logo").removeClass("logo-shrink")
@@ -22,6 +33,12 @@ function scrollFunction() {
 		$("nav h1").removeClass('scroll-h1');
 		$("div.menu").removeClass('menu-right');
 		$("div.menu a").removeClass('menu-item');
+		$("article div.officer-header").removeClass('header-scroll');
+
+		// while (nav_height < 420) {
+		// 	nav_primary.height(nav_height + 2);
+		// 	nav_height += 2;
+		// }
 	}
 }
 
@@ -35,3 +52,31 @@ Method: Create template in the html file and use importNode function to
 
 ----------------------------------------------------------------------------
  */
+
+var execs_display = document.getElementById('executive-officers');
+var secondary_display = document.getElementById('secondary-officers');
+
+var officers = [
+	{'name': 'Asril Duta Hariandy', 'rank': 'President'},
+	{'name': 'Farras Irsyaad Rivai', 'rank': 'Vice President'},
+	{'name': 'Meddelin Trinita Setiawan', 'rank': 'Secretary'},
+	{'name': 'Prawira Pikanto', 'rank': 'Treasurer'},
+	{'name': 'Victor Suhendra', 'rank': 'Public Relations'},
+	{'name': 'Timothy Alexander', 'rank': 'Webmaster'}
+];
+
+for (var i = 0; i < officers.length; i++) {
+	var officer = officers[i];
+	var tmpl = document.getElementById('officer_display').content.cloneNode(true);
+	tmpl.querySelector('.name_container').innerText = officer.name;
+	tmpl.querySelector('.rank_container').innerText = officer.rank;
+
+	if (i < 2) {
+		execs_display.appendChild(tmpl);
+	}
+	else {
+		secondary_display.appendChild(tmpl);
+	}
+
+
+}
